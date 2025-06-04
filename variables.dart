3351dbacec23a4a9670000682277445f281f7558
Vannexs <67067327+Vannexs@users.noreply.github.com>
2025-06-04 12:38:45 +0000

    First commit

---
+++ variables.dart
@@ -0,0 +1,4 @@
+void main(){
+int age =30; //Initialized with the value 30
+double price = 19.99; //Initialized with the value 19.99
+String name = "Alice in Wonderland"; //Initialized with a string literal
